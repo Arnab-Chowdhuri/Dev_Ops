0567e592ce96b1a6af964f202438e671f0daf589
Arnab Chowdhuri <arnabchowdhuri1@gmail.com>
2024-02-29 20:37:48 +0530

    Editing output.tf and remote_backend.tf

--- CFT_and_Terraform/Terraform/remote_backend_s3.tf
+++ CFT_and_Terraform/Terraform/remote_backend_s3.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "remote-backend-statefile-terraform-bucket1"
+    bucket = "remote-backend-statefile-terraform-bucket-1"
     key    = "Terraform_State_File/terraform.tfstate"
     region = "us-east-2"
   }
